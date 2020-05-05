f710bc3aed5ab826c54b4f824a363107388b7389
alenaosintseva <59421607+alenaosintseva@users.noreply.github.com>
2020-05-05 22:49:33 +0300

    remarks

---
+++ Variables.java
@@ -0,0 +1,21 @@
+public class Variables {
+  public static void main(String[] args) {
+      byte coreCount = 2;
+      short bitness = 77;
+      int romGigaByte = 100;
+      long romKBit = 8000000000L;
+      float cpuFrecuency = 2.6F;
+      double ram = 7.41;
+      boolean iCheck = true;
+      char osName = 'W';
+
+      System.out.println("coreCount=" + coreCount);
+      System.out.println("bitness=" + bitness);
+      System.out.println("romGigaByte=" + romGigaByte);
+      System.out.println("romKBit=" + romKBit);
+      System.out.println("cpuFrecuency=" + cpuFrecuency);
+      System.out.println("ram=" + ram);
+      System.out.println("iCheck=" + iCheck);
+      System.out.println("osName=" + osName);
+  }
+}
